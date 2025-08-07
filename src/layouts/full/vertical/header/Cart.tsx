ddb245fd2e026f9741ed610353bd5c89d8057049
Hussien Tarek <shs.bob.hussien@gmail.com>
2025-08-08 01:24:53 +0300

    filtring

--- src/layouts/full/vertical/header/Cart.tsx
+++ src/layouts/full/vertical/header/Cart.tsx
@@ -1,22 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import React, { useState } from 'react';
-import { sum } from 'lodash';
 import { IconShoppingCart, IconX } from '@tabler/icons-react';
-import { Box, Typography, Badge, Drawer, IconButton, Button, Stack } from '@mui/material';
-import { useSelector } from 'src/store/Store';
-import { Link } from 'react-router';
+import { Box, Typography, Badge, Drawer, IconButton } from '@mui/material';
 import CartItems from './CartItem';
-import { AppState } from 'src/store/Store';
 
 const Cart = () => {
   // Get Products
-  const Cartproduct = useSelector((state: AppState) => state.ecommerceReducer.cart);
-  const bcount = Cartproduct.length > 0 ? Cartproduct.length : '0';
-
-  const checkout = useSelector((state: AppState) => state.ecommerceReducer.cart);
-  const total = sum(checkout.map((product: any) => product.price * product.qty));
-
   const [showDrawer, setShowDrawer] = useState(false);
   const handleDrawerClose = () => {
     setShowDrawer(false);
@@ -46,7 +36,7 @@ const Cart = () => {
           }),
         }}
       >
-        <Badge color="error" badgeContent={bcount}>
+        <Badge color="error">
           <IconShoppingCart size="21" stroke="1.5" />
         </Badge>
       </IconButton>
@@ -82,29 +72,6 @@ const Cart = () => {
         {/* Checkout  */}
         {/* ------------------------------------------- */}
         <Box px={3} mt={2}>
-          {Cartproduct.length > 0 ? (
-            <>
-              <Stack direction="row" justifyContent="space-between" mb={3}>
-                <Typography variant="subtitle2" fontWeight={400}>
-                  Total
-                </Typography>
-                <Typography variant="subtitle2" fontWeight={600}>
-                  ${total}
-                </Typography>
-              </Stack>
-              <Button
-                fullWidth
-                component={Link}
-                to="/apps/ecommerce/eco-checkout"
-                variant="contained"
-                color="primary"
-              >
-                Checkout
-              </Button>
-            </>
-          ) : (
-            ''
-          )}
         </Box>
       </Drawer>
     </Box>
--- src/store/Store.tsx
+++ src/store/Store.tsx
@@ -1,14 +1,6 @@
 // File: src/store/Store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import CustomizerReducer from './customizer/CustomizerSlice';
-import EcommerceReducer from './apps/eCommerce/ECommerceSlice';
-import ChatsReducer from './apps/chat/ChatSlice';
-import NotesReducer from './apps/notes/NotesSlice';
-import EmailReducer from './apps/email/EmailSlice';
-import TicketReducer from './apps/tickets/TicketSlice';
-import ContactsReducer from './apps/contacts/ContactSlice';
-import UserProfileReducer from './apps/userProfile/UserProfileSlice';
-import BlogReducer from './apps/blog/BlogSlice';
 import {
   useDispatch as useAppDispatch,
   useSelector as useAppSelector,
@@ -26,15 +18,7 @@ const persistConfig = {
 };
 
 const rootReducer = combineReducers({
-  customizer: CustomizerReducer,
-  ecommerceReducer: EcommerceReducer,
-  chatReducer: ChatsReducer,
-  emailReducer: EmailReducer,
-  notesReducer: NotesReducer,
-  contactsReducer: ContactsReducer,
-  ticketReducer: TicketReducer,
-  userpostsReducer: UserProfileReducer,
-  blogReducer: BlogReducer,
+  customizer: CustomizerReducer
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
